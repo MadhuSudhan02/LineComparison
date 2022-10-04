011211115338029c9345b9ecff5b06c8a61e6e15
Madhu Sudhan <m02sudhan@gmail.com>
2022-10-04 16:38:33 +0530

    Comparing two line

---
+++ CompareTwoLine.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.line;
+
+import java.util.Scanner;
+
+public class CompareTwoLine {
+    public static void main(String[] args) {
+        System.out.println("Enter the co-ordinates for line 1");
+        Scanner sc = new Scanner(System.in);
+        int a, b, c, d, e, f, g, h;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        double line_1 = CalculateLength.lengthOfLine(a, b, c, d);
+        System.out.println("Enter the co-ordinates for line 2");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        double line_2 = CalculateLength.lengthOfLine(a, b, c, d);
+        String s1 = line_1 + "";
+        String s2 = Double.toString(line_2);
+        System.out.println(s1 + "\t" +s2);
+        System.out.println(s1.compareTo(s2));
+
+    }
+}
